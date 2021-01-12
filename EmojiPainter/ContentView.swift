ae4754d6548431dbb983275d7b3c6565a12153e7
quephird <danielle_kefford@hotmail.com>
2021-01-11 22:03:14 -0800

    Added button to copy to clipboard.

--- EmojiPainter/ContentView.swift
+++ EmojiPainter/ContentView.swift
@@ -19,9 +19,40 @@ struct ContentView: View {
                     }
                 }
             }
-            Button("", action: {
-                self.currentColor.currentColor = .purple
-            })
+            HStack {
+                Button("", action: {
+                    self.currentColor.currentColor = .red
+                }).background(Color.red)
+                Button("", action: {
+                    self.currentColor.currentColor = .orange
+                }).background(Color.orange)
+                Button("", action: {
+                    self.currentColor.currentColor = .yellow
+                }).background(Color.yellow)
+                Button("", action: {
+                    self.currentColor.currentColor = .green
+                }).background(Color.green)
+                Button("", action: {
+                    self.currentColor.currentColor = .blue
+                }).background(Color.blue)
+                Button("", action: {
+                    self.currentColor.currentColor = .purple
+                }).background(Color.purple)
+                Button("", action: {
+                    self.currentColor.currentColor = Color.init(red: 0.5, green: 0.25, blue: 0.0, opacity: 1.0)
+                }).background(Color.init(red: 0.5, green: 0.25, blue: 0.0, opacity: 1.0))
+                Button("", action: {
+                    self.currentColor.currentColor = .white
+                }).background(Color.white)
+                Button("", action: {
+                    self.currentColor.currentColor = .black
+                }).background(Color.black)
+                Button("📋", action: {
+                    let pasteboard = NSPasteboard.general
+                    pasteboard.clearContents()
+                    pasteboard.setString("🟥🟧🟨🟩🟦🟪🟫⬛️⬜️", forType: .string)
+                })
+            }
         }.frame(width: 260, height: 390)
     }
 }
