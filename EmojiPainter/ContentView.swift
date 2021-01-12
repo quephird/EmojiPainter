c6ad55f2fe99ba20db351461fd037ae8a47b7e38
quephird <danielle_kefford@hotmail.com>
2021-01-11 19:14:47 -0800

    Added all desired buttons.

--- EmojiPainter/ContentView.swift
+++ EmojiPainter/ContentView.swift
@@ -8,12 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var currentColor: Color = .black
+
     var body: some View {
-        VStack(alignment: .center) {
-            // TODO: Add two hundred buttons which can be clicked!
+        VStack {
+            ForEach(0..<12) { idx in
+                HStack(alignment: .center) {
+                    ForEach(0..<10) { idx in
+                        Button("", action: {
+                            print("I was clicked")
+                        })
+                    }
+                }
+            }
             Button("", action: {
-                print("I was clicked!!!")
+                self.currentColor = .purple
             })
-        }.frame(width: 250, height: 350).border(Color.black)
+        }.frame(width: 250, height: 350)
     }
 }
